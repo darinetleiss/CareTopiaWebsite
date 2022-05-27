f7ec3d922ce4f3389bf633b7adef9e416a757c82
Noor-Hajjaj <85348915+Noor-Hajjaj@users.noreply.github.com>
2022-05-27 19:04:43 +0300

    login linked ma3 back w el token is being saved for the client

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,31 +1,68 @@
-import { createContext, useEffect, useReducer } from "react";
-import AuthReducer from "./authReducer";
+// import { createContext, useEffect, useReducer } from "react";
+// import AuthReducer from "./authReducer";
 
-const INITIAL_STATE = {
-  ngo: JSON.parse(localStorage.getItem("ngo")) || null,
-  isFetching: false,
-  error: false,
-};
+// const INITIAL_STATE = {
+//   ngo: JSON.parse(localStorage.getItem("ngo")) || null,
+//   isFetching: false,
+//   error: false,
+// };
 
-export const AuthContext = createContext(INITIAL_STATE);
+// export const AuthContext = createContext(INITIAL_STATE);
 
-export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+// export const AuthContextProvider = ({ children }) => {
+//   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
-useEffect(() => {
-    localStorage.setItem("ngo", JSON.stringify(state.ngo));
-  }, [state.ngo]);
+// useEffect(() => {
+//     localStorage.setItem("ngo", JSON.stringify(state.ngo));
+//   }, [state.ngo]);
+
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         ngo : state.ngo,
+//         isFetching: state.isFetching,
+//         error: state.error,
+//         dispatch,
+//       }}
+//     >
+//       {children}
+//     </AuthContext.Provider>
+//   );
+//     }
+import React, { useState } from "react";
+
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn:false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  const userIsLoggedIn = !!token;
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn:userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
 
   return (
-    <AuthContext.Provider
-      value={{
-        ngo : state.ngo,
-        isFetching: state.isFetching,
-        error: state.error,
-        dispatch,
-      }}
-    >
-      {children}
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
     </AuthContext.Provider>
   );
-    }
+};
+
+export default AuthContext;
