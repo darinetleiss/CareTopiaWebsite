4360fdf9ad759b1009467861bc86b10d72ee9d9a
darine <darinetleiss733@gmail.com>
2022-05-30 12:51:16 +0300

    token1

--- src/pages/updateProfile/updateProfile.jsx
+++ src/pages/updateProfile/updateProfile.jsx
@@ -46,7 +46,7 @@ export default function UpdateProfile() {
         {
           headers: {
             Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMGQxYmE0ZTgxMDQyMDU1ZDAxNDVmNSIsImlhdCI6MTY1Mzc0NDM3N30.sXlytBA3b11haSu1g2OfStITWOP5RLwigBIiLao7r3U",
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMGQxYmE0ZTgxMDQyMDU1ZDAxNDVmNSIsImlhdCI6MTY1MzkwMzIxOH0.X3WaCJPayeHoSCMzMfFdDGEQowCXnvhFb3mRMjZIix0",
           },
         }
       );
